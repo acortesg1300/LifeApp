bdbe2b60c4b356ae02e562f6c5c6ddb9b0e0c9ab
acortesg1300 <129693697+acortesg1300@users.noreply.github.com>
2023-04-28 21:29:50 -0600

    FIrst coding session
    Learning about classes and stuff. I can barely run my code. Most of the time is bugged. haha.

---
+++ app/src/main/java/com/corderoindustries/corderoapp/Cordero.java
@@ -0,0 +1,16 @@
+package com.corderoindustries.corderoapp;
+
+public class Cordero {
+    boolean isActivity = false;
+
+    public static void main(String[] args) {
+
+
+        Item leche;
+        leche = new Item("Leche", 6, 1, 14);
+
+        String itemAdded = "Se ha agregado " + leche.getName() + "; Costo:" + leche.getCost() + " Prioridad:" + leche.getPriority();
+        System.out.println("Se ha agregado " + leche.getName() + "; Costo:" + leche.getCost() + " Prioridad:" + leche.getPriority());
+
+    }
+}
---
+++ app/src/main/java/com/corderoindustries/corderoapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.corderoindustries.corderoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+
+public class MainActivity extends AppCompatActivity Cordero {
+    TextView app_name;
+    TextView title1;
+    TextView title2;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
