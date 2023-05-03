ea777398dad0ccf5696c91068092b553dbb7b8a5
acortesg1300 <129693697+acortesg1300@users.noreply.github.com>
2023-05-03 09:25:25 -0600

    Erreglado error de folder

---
+++ app/src/main/java/com/corderoindustries/corderoapp/GetNextPayment.java
@@ -0,0 +1,9 @@
+package com.corderoindustries.corderoapp;
+
+public class GetNextPayment extends MainActivity {
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
---
+++ app/src/main/java/com/corderoindustries/corderoapp/Item.java
@@ -0,0 +1,61 @@
+package com.corderoindustries.corderoapp;
+import java.time.LocalDate;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+
+
+public class Item extends Cordero  { // Declaro la clase Item, que es tiene numero unico, nombre, costo prioridad e intervalo
+    @PrimaryKey
+    private String name;
+    @ColumnInfo
+    private float cost;
+    @ColumnInfo
+    private int priority;
+    @ColumnInfo
+    private int interval;
+    ;
+
+
+    public Item(String name, float cost, int priority, int interval){ // declaro la funcion additem, que es una funcion de la clase (funcion) AppCordero, que es la principal.
+        this.name=name;
+        this.cost=cost;
+        this.priority=priority;
+        this.interval=interval;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+}
